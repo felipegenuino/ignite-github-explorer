3867ca7a003ec772e97f22c6fd4c6389826b97f9
Felipe Genuino <design@felipegenuino.com>
2021-07-03 00:18:51 -0300

    listando repositórios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem({repository}){
-   const {name, description, link} = repository 
+   const {name, description, html_url} = repository 
     return(
         <li>
             <strong>{name ?? 'Título do repositório'}</strong>
             <p>{description}</p>
-            <a href="{link}">Acessar repositório {link}</a>
+            <a href={html_url} target="_blank">Acessar repositório {html_url}</a>
         </li>
     )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,37 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+//https://api.github.com/users/felipegenuino/repos
+
 const repository = {
     name: 'unform',
     description: 'Forms in React',
     link: 'https://github.com/unform/unform'
 }
 
+
+
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+//useEffect dispara uma ação quando algo acontece na minha aplicação
+// dependencias, é quando ela for alterada dispara o useEffect
+useEffect( ()=> {
+    fetch('https://api.github.com/users/felipegenuino/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+    // .then(data => console.log(data))
+}, [])
+
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => ( 
+                    <RepositoryItem key={repository.id} repository={repository} />
+                ))} 
             </ul>
         </section>
     )
